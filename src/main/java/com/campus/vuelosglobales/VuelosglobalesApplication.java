23caff1f12da6a36e0860751f61140dfc2472631
lumigamher <lmiguelgamboa@uts.edu.co>
2024-06-19 16:50:51 -0500

    chore: :tada: Initial commit
    Initializing the docker container for the database in mySql

---
+++ src/main/java/com/campus/vuelosglobales/VuelosglobalesApplication.java
@@ -0,0 +1,13 @@
+package com.campus.vuelosglobales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelosglobalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuelosglobalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campus/vuelosglobales/VuelosglobalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.vuelosglobales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelosglobalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
